4e06f2bc26662b9f29075a4ac2975117cdf83774
MyWebAlex <muhachev-2012@mail.ru>
2020-01-24 14:22:14 +0300

    урок 2, лёгкий уровень

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
 let money = 48030, 
     income = 'freelance', 
-    addExpenses = 'water, gas, power', 
+    addExpenses = 'Water, Gas, Power', 
     deposit = true, 
     mission = 1000000, 
-    period = 12;
+    period = 12,
+    budgetDay = money / 30;
+
+console.log(typeof money + '\n' + typeof income + '\n' + typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен '+ period + ' месяцев' + '\n' 
+            + 'Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('budgetDay: ', budgetDay);
