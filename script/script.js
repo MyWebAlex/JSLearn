210a62aee74c8df0e7034db08c907ec2c07cdcde
MyWebAlex <muhachev-2012@mail.ru>
2020-01-24 14:44:14 +0300

    Внёс изменения из вестки lesson2.1 в master

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
-let money = 0, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
-alert('На часах час пятнадцать ночи');
-console.log('И я жутко хочу cпаать');
+let money = 48030, 
+    income = 'freelance', 
+    addExpenses = 'Water, Gas, Power', 
+    deposit = true, 
+    mission = 1000000, 
+    period = 12,
+    budgetDay = money / 30,
+    num = 266219;
+
+let strNum = String(num),
+    newNum = 1;
+for (let i = 0; i < strNum.length; i++)
+    newNum *= +strNum[i];
+console.log('Произведение цифр переменной num: ', newNum);
+newNum **= 3;
+strNum = String(newNum);
+console.log(strNum[0] + strNum[1]);
