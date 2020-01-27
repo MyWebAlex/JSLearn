40a135376494ccdf13d6d06a1b0cdaae127d0b2a
MyWebAlex <muhachev-2012@mail.ru>
2020-01-27 14:44:31 +0300

    Revert "Как про сделал усложнёнку 3 задания"
    This reverts commit ed49e4c0605360a0a7fc21e2daa0374833614e16.

--- script/script.js
+++ script/script.js
@@ -1,60 +1,60 @@
-// 'use strict';
-// let money = 32635, 
-//     income = 'freelance', 
-//     addExpenses = 'Water, Gas, Power', 
-//     deposit = true, 
-//     mission = 1000000, 
-//     period = 12,
-//     budgetDay = money / 30,
-//     expenses1 = '',
-//     expenses2 = '',
-//     amount1 = 0,
-//     amount2 = 0,
-//     budgetMonth = 0;
+'use strict';
+let money = 32635, 
+    income = 'freelance', 
+    addExpenses = 'Water, Gas, Power', 
+    deposit = true, 
+    mission = 1000000, 
+    period = 12,
+    budgetDay = money / 30,
+    expenses1 = '',
+    expenses2 = '',
+    amount1 = 0,
+    amount2 = 0,
+    budgetMonth = 0;
 
-// console.log(typeof money + '\n' + typeof income + '\n' + typeof deposit);
-// console.log(addExpenses.length);
-// console.log('Период равен '+ period + ' месяцев' + '\n' 
-//             + 'Цель заработать ' + mission + ' рублей');
-// console.log(addExpenses.toLowerCase().split(', '));
-// console.log('budgetDay: ', budgetDay);
+console.log(typeof money + '\n' + typeof income + '\n' + typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен '+ period + ' месяцев' + '\n' 
+            + 'Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('budgetDay: ', budgetDay);
 
-// //Задание 2
-// while (!(money = parseInt(prompt('Какой Ваш месячный доход?')))) { }
-// //Задание 3
-// addExpenses = prompt('Перечислите возможные расходы за рассчитываемый' 
-//                     + 'период через запятую').split(', ');
-// // Задание 4
-// deposit = confirm('Есть ли у вас депозит в банке?');
+//Задание 2
+while (!(money = parseInt(prompt('Какой Ваш месячный доход?')))) { }
+//Задание 3
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый' 
+                    + 'период через запятую').split(', ');
+// Задание 4
+deposit = confirm('Есть ли у вас депозит в банке?');
 
-// //задание 5
-// expenses1 = prompt('Введите обязательную статью расходов.');
-// while (!(amount1 = parseInt(prompt('Во сколько это обойдется?')))) { }
-// expenses2 = prompt('Введите обязательную статью расходов.');
-// while (!(amount2 = parseInt(prompt('Во сколько это обойдется?')))) { }
+//задание 5
+expenses1 = prompt('Введите обязательную статью расходов.');
+while (!(amount1 = parseInt(prompt('Во сколько это обойдется?')))) { }
+expenses2 = prompt('Введите обязательную статью расходов.');
+while (!(amount2 = parseInt(prompt('Во сколько это обойдется?')))) { }
 
-// //задание 6
-// budgetMonth = money - amount1 - amount2;
-// console.log('Остаток на месяц:', budgetMonth);
+//задание 6
+budgetMonth = money - amount1 - amount2;
+console.log('Остаток на месяц:', budgetMonth);
 
-// //задание 7
-// console.log('Месяцев на достижение цели:', Math.ceil(mission / budgetMonth) );
+//задание 7
+console.log('Месяцев на достижение цели:', Math.ceil(mission / budgetMonth) );
 
-// //задание 8
-// budgetDay = Math.floor(budgetMonth / 30);
+//задание 8
+budgetDay = Math.floor(budgetMonth / 30);
 
-// //задание 9
-// if      (budgetDay > 1200)
-//     console.log('У вас высокий уровень дохода');
-// else if (budgetDay === 1200)
-//     console.log('У вас высокий уровень дохода');
-// else if (budgetDay > 600)
-//     console.log('У вас средний уровень дохода');
-// else if (budgetDay === 600)
-//     console.log('У вас средний уровень дохода');
-// else if (budgetDay > 0)
-//     console.log('К сожалению у вас уровень дохода ниже среднего');
-// else if (budgetDay === 0)
-//     console.log('Вы живёте впритык');
-// else
-//     console.log('В вашей жизни Что-то пошло не так');
+//задание 9
+if      (budgetDay > 1200)
+    console.log('У вас высокий уровень дохода');
+else if (budgetDay === 1200)
+    console.log('У вас высокий уровень дохода');
+else if (budgetDay > 600)
+    console.log('У вас средний уровень дохода');
+else if (budgetDay === 600)
+    console.log('У вас средний уровень дохода');
+else if (budgetDay > 0)
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+else if (budgetDay === 0)
+    console.log('Вы живёте впритык');
+else
+    console.log('В вашей жизни Что-то пошло не так');
