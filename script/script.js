12784057be303157d8f9f65fff05238c6c793c71
MyWebAlex <muhachev-2012@mail.ru>
2020-01-27 14:58:48 +0300

    убрал номера заданий

--- script/script.js
+++ script/script.js
@@ -14,31 +14,23 @@ console.log('Период равен '+ period + ' месяцев' + '\n'
 console.log(addExpenses.toLowerCase().split(', '));
 console.log('budgetDay: ', budgetDay);
 
-//Задание 2
 while ( !(money = parseInt( prompt('Какой Ваш месячный доход?') )) ) { }
-//Задание 3
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый' 
                     + 'период через запятую').split(', ');
-// Задание 4
 deposit = confirm('Есть ли у вас депозит в банке?');
 
-//задание 5
 expenses1 = prompt('Введите обязательную статью расходов.');
 while ( !(amount1 = parseInt( prompt('Во сколько это обойдется?')) ) ) { }
 expenses2 = prompt('Введите обязательную статью расходов.');
 while ( !(amount2 = parseInt( prompt('Во сколько это обойдется?') )) ) { }
 
-//задание 6
 budgetMonth = money - amount1 - amount2;
 console.log('Остаток на месяц:', budgetMonth);
 
-//задание 7
 console.log('Месяцев на достижение цели:', Math.ceil(mission / budgetMonth) );
 
-//задание 8
 budgetDay = Math.floor(budgetMonth / 30);
 
-//задание 9
 if      (budgetDay > 1200)
     console.log('У вас высокий уровень дохода');
 else if (budgetDay === 1200)
