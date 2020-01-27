e6fa2c40d886590a612162c769e09757376b3682
MyWebAlex <muhachev-2012@mail.ru>
2020-01-27 22:06:25 +0300

    Изуродовал свой код (хотя точечная нотация ассоциативных массивов норм)

--- script/lesson3.2.js
+++ script/lesson3.2.js
@@ -4,28 +4,36 @@ let lang = document.getElementsByTagName('html')[0].getAttribute('lang');
 let weekDays = {'en':['Sunday', 'Monday', 'Tuesday', 'Wednesday', 
                 'Thursday', 'Friday', 'Saturday'], 
                 'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 
-                'Пятница', 'Суббота', 'Воскресенье']}
+                'Пятница', 'Суббота', 'Воскресенье']};
 let li = document.getElementsByTagName('li');
 
 //Способ a
-if (lang === 'en')
-    for (let i = 0; i < 7; i++)
-        li[i].innerHTML = weekDays['en'][i];
-else if (lang === 'ru')
-    for (let i = 0; i < 7; i++)
-        li[i].innerHTML = weekDays['ru'][i];
-else 
+if (lang === 'en') {
+    for (let i = 0; i < 7; i++) {
+        li[i].innerHTML = weekDays.en[i];
+    }
+}
+else if (lang === 'ru') {
+    for (let i = 0; i < 7; i++) {
+        li[i].innerHTML = weekDays.ru[i];
+    } 
+}
+else {
     alert('Страница имеет неверный язык');
+}
+    
 
 //Способ b
 switch (lang) {
     case 'en':
-        for (let i = 0; i < 7; i++)
-            li[i].innerHTML = weekDays['en'][i];
+        for (let i = 0; i < 7; i++) {
+            li[i].innerHTML = weekDays.en[i];
+        }
         break;
     case 'ru':
-        for (let i = 0; i < 7; i++)
-            li[i].innerHTML = weekDays['ru'][i];
+        for (let i = 0; i < 7; i++) {
+            li[i].innerHTML = weekDays.ru[i];
+        }
         break;
     default:
         alert('Страница имеет неверный язык');
@@ -33,8 +41,9 @@ switch (lang) {
 }
 
 //Способ c
-for (let i = 0; i < 7; i++)
+for (let i = 0; i < 7; i++) {
     li[i].innerHTML = weekDays[lang][i];
+}
 
 //Задание 2
 let namePerson = 'Саня';
