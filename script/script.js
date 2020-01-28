541353664f2df79667a438b3da46ef0f2c6f98b8
MyWebAlex <muhachev-2012@mail.ru>
2020-01-28 19:27:38 +0300

    Сделал Задание 4.1

--- script/script.js
+++ script/script.js
@@ -1,51 +1,83 @@
-'use strict';
-// Заранее заданные переменные
-let mission = 1000000;
-let period = 12;
-let income = '';
+'use strict'; // Lkx строгой проверки кода
 
-// Вводим зароботок
-let money;
-while ( !(money = parseInt( prompt('Какой Ваш месячный доход?', 35000) )) ) { let n = true; }
 
-// Вводим возможные расходы
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Water, Gas, Power');
+// Блок функций //
+// Функция, рассчитывающая все доходы
+const getIncomeMonth = function(income1) { // Позже добавить income2
+    return income1;
+};
+// Функция, рассчитывающая все расходы
+const getExpensesMonth = function(expense1, expense2) {
+    return expense1 + expense2;
+};
+// Функция, рассчитывающая остаток в месяц после доходов и расходов
+const getAccumulatedMonth = function(incomes, expenses) {
+    return incomes - expenses;
+};
+// Функция, рассчитывающая количество месяцев до достижения цели
+const getTargetMonth = function(accumulatedMonth, mission) {
+    return Math.ceil(mission / accumulatedMonth);
+};
+// Функция, рассчитывающая остаток в день после доходов и расходов
+const getAccumulatedDay = function(accumulatedMonth) {
+    return Math.floor(accumulatedMonth / 30);
+};
+// Функция, выводящая в консоль тип переменной
+const showTypeOf = function(variable) {
+    console.log(typeof variable, variable);
+};
+// Функция, отпределяющая уровень доходов
+const getStatusIncome = function(accumulatedDay) {
+    if (accumulatedDay >= 1200) { return 'У вас высокий уровень дохода'; }
+    else if (accumulatedDay >= 600) { return 'У вас средний уровень дохода'; }
+    else if (accumulatedDay >= 0) { return 'К сожалению у вас уровень дохода ниже среднего'; }
+    else { return 'В вашей жизни Что-то пошло не так'; }
+};
+// Функция, которая через prompt требует число
+const getOnlyNumberFromPrompt = function(message, defaultMessage) {
+    let tmp;
+    while ( !(tmp = parseInt( prompt(message, defaultMessage) )) ) { let n = true; }
+    return tmp;
+};
+// Функция, которая разбивает строку на подстроки через ', ' и приводит все символы к нижнему регистру
+const getArrayFromString = function(str) {
+    str = str.toLowerCase();
+    str = str.split(', ');
+    return str;
+};
+// Блок функций Конец! //
 
-// Есть ли депозит?
-let deposit = confirm('Есть ли у вас депозит в банке?');
 
-// Ввод обязательных статей расходов
-let expenses1 = prompt('Введите обязательную статью расходов.', 'Studying');
-let amount1;
-while ( !(amount1 = parseInt( prompt('Во сколько это обойдется?', 13000)) ) ) { let n = true; }
-let expenses2 = prompt('Введите обязательную статью расходов.', 'Food');
-let amount2;
-while ( !(amount2 = parseInt( prompt('Во сколько это обойдется?', 10000) )) ) { let n = true; }
+// Блок переменных //
+const mission = 1000000; // Сколько хотим заработать
+const money = getOnlyNumberFromPrompt('Какой Ваш месячный доход?', 35000); // Доход в месяц
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
+'Вода, Газ, Электричество'); // Дополнительные расходы
+const deposit = confirm('Есть ли у вас депозит в банке?'); // Есть ли депозит?
+const expenses1 = prompt('Введите обязательную статью расходов.', 'Учёба'); // Осн. расходы 1
+const amount1 = getOnlyNumberFromPrompt('Во сколько это обойдется?', 13000); // Осн. расходы 1
+const expenses2 = prompt('Введите обязательную статью расходов.', 'Еда'); // Осн. расходы 2
+const amount2 = getOnlyNumberFromPrompt('Во сколько это обойдется?', 10000); // Осн. расходы 2
+const income = prompt('Введите дополнительную статью доходов, если есть.', 'Фриланс'); // Допольнительный доход
+// Блок переменных Конец! //
 
-// Рассчитываем остаток от месячного бюджета после трат
-let budgetMonth = money - amount1 - amount2;
 
+// Блок Программы //
+// Разбиваем строку трат на массив
+addExpenses = getArrayFromString(addExpenses);
+// Рассчитывем чистый доход в месяц
+const accumulatedMonth = getAccumulatedMonth( getIncomeMonth(money), getExpensesMonth(amount1, amount2) );
 // Рассчитываем дневной остаточный бюджет
-let budgetDay = Math.floor(budgetMonth / 30);
+const budgetDay = getAccumulatedDay(accumulatedMonth);
 
-// Подготавливаем переменные к выводу
-let addExpensesCounter = addExpenses.length;
-addExpenses = addExpenses.split(', ');
-for (let i = 0; i < addExpenses.length; i++) {
-    addExpenses[i] = addExpenses[i].toLowerCase();
-}
-let budgetMessage;
-if (budgetDay >= 1200) { budgetMessage = 'У вас высокий уровень дохода'; }
-else if (budgetDay >= 600) { budgetMessage = 'У вас средний уровень дохода'; }
-else if (budgetDay >= 0) { budgetMessage = 'К сожалению у вас уровень дохода ниже среднего'; }
-else { budgetMessage = 'В вашей жизни Что-то пошло не так'; }
 
 // Выводим все необходимые данные в консоль
-console.log(typeof money + '\n' + typeof income + '\n' + typeof deposit);
-console.log(addExpensesCounter);
-console.log('Период равен '+ period + ' месяцев' + '\n' + 'Цель заработать ' + mission + ' рублей');
-console.log(addExpenses);
-console.log('Остаток на месяц:', budgetMonth);
-console.log('Месяцев на достижение цели:', Math.ceil(mission / budgetMonth) );
-console.log('Бюджет на день: ', budgetDay);
-console.log(budgetMessage);
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+console.log('Основные траты за месяц:', getExpensesMonth(amount1, amount2));
+console.log('Дополнительные траты:', addExpenses);
+console.log('Месяцев до выполнения цели:', getTargetMonth(accumulatedMonth, mission));
+console.log('Бюджет на день:', budgetDay);
+console.log(getStatusIncome(budgetDay));
+// Блок Программы Конец! //
