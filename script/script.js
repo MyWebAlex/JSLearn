7f4c58bbfcca71b8bf28132f89765b23026c860f
MyWebAlex <muhachev-2012@mail.ru>
2020-01-31 23:54:41 +0300

    Я поборол это д*

--- script/script.js
+++ script/script.js
@@ -1,105 +1,40 @@
-'use strict'; // Для строгой проверки кода
+'use strict';
 
+//const number = Math.floor(Math.random() * 100) + 1;
 
-// Блок функций //
-// Функция, рассчитывающая все доходы
-const getIncomeMonth = function(income1) { // Позже добавить income2
-    return income1;
-};
-// Функция, рассчитывающая все расходы
-const getExpensesMonth = function(expenses) {
-    let tmp = 0;
-    for (let i = 0; i < expenses.length; i++) {
-        tmp += expenses[i];
+function start(number) {
+    console.log('number: ', number);
+    let counter = 10;
+
+    function restart() {
+        counter = 10;
+        return start(Math.floor(Math.random() * 100) + 1)(prompt('Угадай число от 1 до 100'));
     }
-    return tmp;
-};
-// Функция, рассчитывающая остаток в месяц после доходов и расходов
-const getAccumulatedMonth = function(incomes, expenses) {
-    return incomes - expenses;
-};
-// Функция, рассчитывающая количество месяцев до достижения цели
-const getTargetMonth = function(accumulatedMonth, mission) {
-    let tmp = Math.ceil(mission / accumulatedMonth);
-    return tmp > 0 ? tmp : 'Цель не может быть достигнута';
-};
-// Функция, рассчитывающая остаток в день после доходов и расходов
-const getAccumulatedDay = function(accumulatedMonth) {
-    return Math.floor(accumulatedMonth / 30);
-};
-// Функция, выводящая в консоль тип переменной
-const showTypeOf = function(variable) {
-    console.log(typeof variable, variable);
-};
-// Функция, отпределяющая уровень доходов
-const getStatusIncome = function(accumulatedDay) {
-    if (accumulatedDay >= 1200) { return 'У вас высокий уровень дохода'; }
-    else if (accumulatedDay >= 600) { return 'У вас средний уровень дохода'; }
-    else if (accumulatedDay >= 0) { return 'К сожалению у вас уровень дохода ниже среднего'; }
-    else { return 'В вашей жизни Что-то пошло не так'; }
-};
-// Функция, которая через prompt требует число
-const getOnlyNumberFromPrompt = function(message, defaultValue) {
-    let tmp;
-    do { 
-        tmp = parseInt( prompt(message, defaultValue) );
-    } while ( !tmp );
-    return tmp;
-};
-// функция, которая принимает текст из prompt
-const getPrompt = function(message, defaultValue) {
-    let tmp;
-    do { 
-        tmp = prompt(message, defaultValue);
-    } while ( !tmp );
-    return tmp;
-};
-// Функция, которая разбивает строку на подстроки через ', ' и приводит все символы к нижнему регистру
-const getArrayFromString = function(str) {
-    str = str.toLowerCase();
-    str = str.split(', ');
-    return str;
-};
-// Блок функций Конец! //
 
+    return function play(tryNumber) {
+        if (tryNumber === null) {
+            return confirm('Вы уверены, что хотите выйти?') ? alert('Ок. Удачи') : restart();
+        }
+        tryNumber = +tryNumber;
+        if (!tryNumber || tryNumber < 0 || tryNumber > 100) {
+            return play(prompt('Введите число от 1 до 100'));
+        }
+        if (tryNumber === number) {
+            return confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') ? restart() : alert('Ок. Удачи');
+        }
+        counter--;
+        if (counter === 0) {
+            return confirm('Попытки закончились, хотите сыграть еще?') ? restart() : alert('Ок. Удачи');
+        }
+        if (tryNumber > number) {
+            return play(prompt('Загаданное число меньше, попыток осталось ' + counter));
+        }
+        if (tryNumber < number) {
+            return play(prompt('Загаданное число больше, попыток осталось ' + counter));
+        }
+    };
 
-// Блок переменных //
-const mission = 1000000; // Сколько хотим заработать
-let money, // Доход в месяц
-    addExpenses, // Дополнительные расходы
-    deposit, // Есть ли депозит?
-    income, // Допольнительный доход
-    expenses = [ ], // Основные расходы - название
-    amounts = [ ]; // Основные расходы - значения
-// Блок переменных Конец! //
-
-
-// Блок Программы //
-// Вписываем данные
-money = getOnlyNumberFromPrompt('Какой Ваш месячный доход?', 35000);
-addExpenses = getPrompt('Перечислите возможные расходы за рассчитываемый период через запятую',
-'Вода, Газ, Электричество');
-deposit = confirm('Есть ли у вас депозит в банке?');
-income = prompt('Введите дополнительную статью доходов, если есть.', 'Фриланс');
-for (let i = 0; i < 2; i++) {
-    expenses[i] = getPrompt('Введите обязательную статью расходов.', i === 0 ? 'Учёба' : 'Еда');
-    amounts[i] = getOnlyNumberFromPrompt('Во сколько это обойдется?', i === 0 ? 13000 : 10000);
 }
 
-// Разбиваем строку трат на массив
-addExpenses = getArrayFromString(addExpenses);
-// Рассчитывем чистый доход в месяц
-const accumulatedMonth = getAccumulatedMonth( getIncomeMonth(money), getExpensesMonth(amounts) );
-// Рассчитываем дневной остаточный бюджет
-const budgetDay = getAccumulatedDay(accumulatedMonth);
-
-// Выводим все необходимые данные в консоль
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-console.log('Основные траты за месяц:', getExpensesMonth(amounts));
-console.log('Дополнительные траты:', addExpenses);
-console.log('Месяцев до выполнения цели:', getTargetMonth(accumulatedMonth, mission));
-console.log('Бюджет на день:', budgetDay);
-console.log(getStatusIncome(budgetDay));
-// Блок Программы Конец! //
+const game = start(Math.floor(Math.random() * 100) + 1);
+game(prompt('Угадай число от 1 до 100'));
