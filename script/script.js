c4e25748dbb8076fc5d6361b85a5b9ac7f302399
MyWebAlex <muhachev-2012@mail.ru>
2020-02-01 21:59:32 +0300

    даунгрейд в задание 6.1

--- script/script.js
+++ script/script.js
@@ -4,33 +4,31 @@
 
 function start(number) {
     console.log('number: ', number);
-    let counter = 10;
 
     function restart() {
-        counter = 10;
         return start(Math.floor(Math.random() * 100) + 1)(prompt('Угадай число от 1 до 100'));
+    }
+
+    function gameOver(overMessage) {
+        return alert(overMessage);
     }
 
     return function play(tryNumber) {
         if (tryNumber === null) {
-            return confirm('Вы уверены, что хотите выйти?') ? alert('Ок. Удачи') : restart();
+            return gameOver('Ок. Удачи!');
         }
         tryNumber = +tryNumber;
         if (!tryNumber || tryNumber < 0 || tryNumber > 100) {
             return play(prompt('Введите число от 1 до 100'));
         }
         if (tryNumber === number) {
-            return confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') ? restart() : alert('Ок. Удачи');
-        }
-        counter--;
-        if (counter === 0) {
-            return confirm('Попытки закончились, хотите сыграть еще?') ? restart() : alert('Ок. Удачи');
+            return gameOver('Поздравляю, Вы угадали!!!');
         }
         if (tryNumber > number) {
-            return play(prompt('Загаданное число меньше, попыток осталось ' + counter));
+            return play(prompt('Загаданное число меньше'));
         }
         if (tryNumber < number) {
-            return play(prompt('Загаданное число больше, попыток осталось ' + counter));
+            return play(prompt('Загаданное число больше'));
         }
     };
 
