05b85c4903bf0672ccb63543d6d4ee658fbf77fb
MyWebAlex <muhachev-2012@mail.ru>
2020-02-01 22:07:30 +0300

    добавил функцию gameOver

--- script/script.js
+++ script/script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//const number = Math.floor(Math.random() * 100) + 1;
-
 function start(number) {
     console.log('number: ', number);
     let counter = 10;
@@ -10,21 +8,24 @@ function start(number) {
         counter = 10;
         return start(Math.floor(Math.random() * 100) + 1)(prompt('Угадай число от 1 до 100'));
     }
+    function gameOver(overMessage) {
+        return alert(overMessage);
+    }
 
     return function play(tryNumber) {
         if (tryNumber === null) {
-            return confirm('Вы уверены, что хотите выйти?') ? alert('Ок. Удачи') : restart();
+            return confirm('Вы уверены, что хотите выйти?') ? gameOver('Ок. Удачи') : restart();
         }
         tryNumber = +tryNumber;
         if (!tryNumber || tryNumber < 0 || tryNumber > 100) {
             return play(prompt('Введите число от 1 до 100'));
         }
         if (tryNumber === number) {
-            return confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') ? restart() : alert('Ок. Удачи');
+            return confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') ? restart() : gameOver('Ок. Удачи');
         }
         counter--;
         if (counter === 0) {
-            return confirm('Попытки закончились, хотите сыграть еще?') ? restart() : alert('Ок. Удачи');
+            return confirm('Попытки закончились, хотите сыграть еще?') ? restart() : gameOver('Ок. Удачи');
         }
         if (tryNumber > number) {
             return play(prompt('Загаданное число меньше, попыток осталось ' + counter));
@@ -37,5 +38,4 @@ function start(number) {
 }
 
 const game = start(Math.floor(Math.random() * 100) + 1);
-console.dir('game: ', game);
 game(prompt('Угадай число от 1 до 100'));
