0d14594c3f4185e6d0f5cec38682f20ccbef4e97
MyWebAlex <muhachev-2012@mail.ru>
2020-02-05 10:46:02 +0300

    сделал 7.2

--- script/lesson7.2.js
+++ script/lesson7.2.js
@@ -4,13 +4,16 @@ const week = [
     'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'
 ];
 
-for (let item of week) {
-    if (item === 'Суббота' || item === 'Воскресенье') {
-        document.writeln(item.italics());
-    } else if (Date.week === item) {
-        document.writeln(item + '+');
-    } else {
-        document.writeln(item);
+let day = new Date().getDay();
+
+for (let i = 0; i < week.length; i++) {
+    let msg = week[i];
+    if (i === 5 || i === 6) {
+        msg = msg.italics();
     }
+    if (day === i) {
+        msg = msg.bold();
+    }
+    document.writeln('<p>' + msg + '</p>');
 
 }
